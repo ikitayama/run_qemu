820f7e3fb830f7e4b528cdbbeee7c476de0e11b4
Itaru Kitayama <itaru.kitayama@linux.dev>
2025-04-01 11:32:37 +0900

    Always set -cpu option on aarch64 otherwise Linux won't boot.

--- run_qemu.sh
+++ run_qemu.sh
@@ -5,8 +5,7 @@
 # default config
 : "${builddir:=./qbuild}"
 rootpw="root"
-#rootfssize="10G"
-rootfssize="7G"
+rootfssize="10G"
 espsize="512M"
 nvme_size="1G"
 efi_mem_size="2"   #in GiB
@@ -1733,7 +1732,13 @@ prepare_qcmd()
 	# For arm64, if not -cpu is explicitly set Linux won't boot
 		qcmd+=("-cpu" "max")
 	fi
-        qcmd+=("-cpu" "cortex-a72")
+
+        # If not "-cpu" option not set, Linux won't boot 
+	#
+	if [[ $(arch) == aarch64 ]]; then
+		qcmd+=("-cpu" "max")
+	fi
+
 	if [[ $_arg_cxl == "on" ]]; then
 		setup_cxl
 	fi
